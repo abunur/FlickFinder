6ee29f5ae4f8c2dd7e585295e0bddf71bbc266b3
Fuad Kamal <fuad@anaara.com>
2025-03-09 13:50:44 -0400

    dummy data
    move dummy data into it’s own file, and comment out. Later can re-structure to match changes to data classes and use for testing

---
+++ app/src/main/java/com/anaara/movieexplorer/data/FakeData.kt
@@ -0,0 +1,40 @@
+package com.anaara.movieexplorer.data
+
+import com.anaara.movieexplorer.data.model.ImdbMovie
+
+/**
+ * Mock data for testing
+ */
+// Mock Movie Data
+//
+//val movies = listOf(
+//    ImdbMovie("Oppenheimer", 2023, "The story of J. Robert Oppenheimer's role in the Manhattan Project.", listOf("Biography", "Drama", "History")),
+//    ImdbMovie("Barbie", 2023, "Barbie and Ken leave Barbie Land and travel to the real world.", listOf("Comedy", "Fantasy")),
+//    ImdbMovie("The Shawshank Redemption", 1994, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", listOf("Drama")),
+//    ImdbMovie("The Dark Knight", 2008, "When the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological tests of his ability to fight injustice.", listOf("Action", "Crime", "Drama")),
+//    ImdbMovie(
+//        "The Lord of the Rings: The Return of the King",
+//        2003,
+//        "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+//        listOf("Action", "Adventure", "Drama")
+//    ),
+//    ImdbMovie(
+//        "The Marvels",
+//        2023,
+//        "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe.",
+//        listOf("Action", "Adventure", "Fantasy")
+//    ),
+//    ImdbMovie(
+//        "Spider-Man: Across the Spider-Verse",
+//        2023,
+//        "Miles Morales catapults across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence. When the heroes clash on how to handle a new threat, Miles must redefine what it means to be a hero.",
+//        listOf("Animation", "Action", "Adventure")
+//    ),
+//    ImdbMovie(
+//        "Guardians of the Galaxy Vol. 3",
+//        2023,
+//        "Still reeling from the loss of Gamora, Peter Quill rallies his team to defend the universe and one of their own - a mission that could mean the end of the Guardians if not successful.",
+//        listOf("Action", "Adventure", "Comedy")
+//    )
+//)
+//
