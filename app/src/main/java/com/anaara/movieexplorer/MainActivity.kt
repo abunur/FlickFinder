b70fccb9d8e34068bc3b92a6a68c6a231a453a40
Fuad Kamal <fuad@anaara.com>
2025-03-09 14:33:51 -0400

    update UI for live data
    update the UI now with live data from the API.
    App now shows live data from the API instead of fake data from original UI mock

--- app/src/main/java/com/anaara/movieexplorer/MainActivity.kt
+++ app/src/main/java/com/anaara/movieexplorer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -32,56 +33,57 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.anaara.movieexplorer.components.MovieCard
+import com.anaara.movieexplorer.data.fakeMovies
 import com.anaara.movieexplorer.data.model.ImdbMovie
+import com.anaara.movieexplorer.viewmodels.MovieViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
-// Mock Movie Data
-val movies = listOf(
-    ImdbMovie("Oppenheimer", 2023, "The story of J. Robert Oppenheimer's role in the Manhattan Project.", listOf("Biography", "Drama", "History")),
-    ImdbMovie("Barbie", 2023, "Barbie and Ken leave Barbie Land and travel to the real world.", listOf("Comedy", "Fantasy")),
-    ImdbMovie("The Shawshank Redemption", 1994, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", listOf("Drama")),
-    ImdbMovie("The Dark Knight", 2008, "When the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological tests of his ability to fight injustice.", listOf("Action", "Crime", "Drama")),
-    ImdbMovie(
-        "The Lord of the Rings: The Return of the King",
-        2003,
-        "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
-        listOf("Action", "Adventure", "Drama")
-    ),
-    ImdbMovie(
-        "The Marvels",
-        2023,
-        "Carol Danvers, aka Captain Marvel, has reclaimed her identity from the tyrannical Kree and taken revenge on the Supreme Intelligence. But unintended consequences see Carol shouldering the burden of a destabilized universe.",
-        listOf("Action", "Adventure", "Fantasy")
-    ),
-    ImdbMovie(
-        "Spider-Man: Across the Spider-Verse",
-        2023,
-        "Miles Morales catapults across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence. When the heroes clash on how to handle a new threat, Miles must redefine what it means to be a hero.",
-        listOf("Animation", "Action", "Adventure")
-    ),
-    ImdbMovie(
-        "Guardians of the Galaxy Vol. 3",
-        2023,
-        "Still reeling from the loss of Gamora, Peter Quill rallies his team to defend the universe and one of their own - a mission that could mean the end of the Guardians if not successful.",
-        listOf("Action", "Adventure", "Comedy")
-    )
-    )
-
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MovieViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieApp()
+            MovieApp(viewModel)
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MovieApp() {
+fun MovieApp(viewModel: MovieViewModel = viewModel()) {
+    val genres by viewModel.genres.collectAsStateWithLifecycle(emptyList())
+    val movies by viewModel.movies.collectAsStateWithLifecycle(emptyList())
+    val selectedGenre by viewModel.selectedGenre.collectAsStateWithLifecycle()
+    val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+
+    MovieList(
+        movies = movies,
+        isLoading = isLoading,
+        onMovieClick = {
+            // TODO: Handle movie click
+        }
+    )
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieList(
+    movies: List<ImdbMovie> = emptyList(),
+    isLoading: Boolean = false,
+    onMovieClick: (ImdbMovie) -> Unit = {},
+) {
+
+
+
     var selectedGenre by remember { mutableStateOf("All") }
     val filteredMovies = if (selectedGenre == "All") {
         movies
@@ -159,7 +161,7 @@ fun MovieApp() {
                     items(filteredMovies) { movie ->
                         MovieCard(
                             title = movie.title,
-                            year = movie.year,
+                            year = movie.getReleaseYear(),
                             overview = movie.overview,
                             genres = movie.genres
                         )
@@ -175,5 +177,5 @@ fun MovieApp() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieApp()
+    MovieList(movies = fakeMovies)
 }
