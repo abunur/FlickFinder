e1369b033a92349247c0aeb011443c3eb7fb1197
Fuad Kamal <fuad@anaara.com>
2025-03-09 17:29:26 -0400

    factory method for future parsing
    factory method to return list of Genre, specifying the type

--- app/src/main/java/com/anaara/movieexplorer/data/model/Genre.kt
+++ app/src/main/java/com/anaara/movieexplorer/data/model/Genre.kt
@@ -4,3 +4,21 @@ data class Genre(
     val name: String,
     val count: Int
 )
+
+//// Data class to represent the list of genres (entire API response)
+//data class GenreResponse(
+//    val genres: List<Genre>
+//) {
+//    companion object {
+//        // Factory method to parse the API response format
+//        fun fromApiResponse(apiResponseArray: List<List<Any>>): GenreResponse {
+//            val genreList = apiResponseArray.map { item ->
+//                Genre(
+//                    name = item[0] as String,
+//                    count = (item[1] as Number).toInt()
+//                )
+//            }
+//            return GenreResponse(genreList)
+//        }
+//    }
+//}
